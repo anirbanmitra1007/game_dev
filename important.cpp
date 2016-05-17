156b9beee748257299070804b0d94ad0bc9e24e8
prototype <prototype>
2016-05-18 00:25:23 +0530

    new macros

--- important.cpp
+++ important.cpp
@@ -1,25 +1,31 @@
 #include <bits/stdc++.h>
-#include <math.h>
 
 using namespace std;
 typedef long long int ll;
 typedef long double ld;
+typedef vector<int> vi; 
+typedef vector<ll> vl; 
+typedef vector<ld> vd; 
+typedef vector<vi> vvi; 
 
-#define IOS     	ios_base::sync_with_stdio(false); 
-#define pb 			push_back
-#define mp      	make_pair
-#define mod 		1000000007
+#define IOS         ios_base::sync_with_stdio(false); 
+#define pb          push_back
+#define mp          make_pair
+#define mod         1000000007
 #define FOR(i,a,b) for(int (i) = (a); (i) <= (b); ++(i))
 #define ROF(i,a,b) for(int (i) = (a); (i) >= (b); --(i))
 #define rep(i,n)   for(int (i) = 0; (i) < (n); ++(i))
-#define mem(i,n)	memset(i,n,sizeof(i))
-#define gcd(a,b)	__gcd(a,b)
-#define sz(a)		((int)(a.size()))
-#define len(a)		((int)a.length())
-#define all(vi)		vi.begin(), vi.end()
-#define mem(i,n)	memset(i,n,sizeof(i))
-#define ff      	first
-#define ss      	second
+#define mem(i,n)    memset(i,n,sizeof(i))
+#define gcd(a,b)    __gcd(a,b)
+#define sz(a)       ((int)(a.size()))
+#define len(a)      ((int)a.length())
+#define all(vi)     vi.begin(), vi.end()
+#define mem(i,n)    memset(i,n,sizeof(i))
+#define ff          first
+#define ss          second
+#define trav(container, it)      for(typeof(container.begin()) it = container.begin(); it != container.end(); it++) 
+#define present(container, element) (container.find(element) != container.end()) 
+#define cpresent(container, element) (find(all(container),element) != container.end()) 	second
 
 typedef vector<vector<ll> > matrix;
 
